71b8155158de1dfb9e776a5f04ce7d9f3469cf9c
R4chel <rae84@cornell.edu>
2021-04-02 15:38:16 -0400

    more edits to deploy script

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,8 @@ if ! git diff --exit-code > /dev/null; then
 	  echo "working directory not clean, exiting." 
 	  exit 1
 fi 
+git checkout -b gh-pages &> /dev/null
+trap "git checkout - &> /dev/null" EXIT
 
 # Build 
 echo "building site..."
@@ -30,9 +32,20 @@ elm make ./src/Main.elm --optimize --output=./$outdir/elm.js
 # uglifyjs ./$outdir/elm.js --compress "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe" | uglifyjs --mangle --output=./$outdir/elm.min.js
 
 
-# Add & Commit & Deploy to GH Pages
-git add .
-git commit -m "rebuild triggered - `date`"
-git subtree push --prefix $outdir origin gh-pages
+#TODO use $outdir not 'dist'
+# Delete and move files.
+find . -maxdepth 1 ! -name 'dist' ! -name '.git' ! -name '.gitignore'  -exec rm -rf {} \;
+mv dist/* .
+rm -R dist/
 
-echo "Done"
+# Push to gh-pages.
+echo "committing compiled site..."
+git add -A > /dev/null
+git commit --allow-empty -m "$(git log -1 --pretty=%B)" > /dev/null
+echo "pushing compiled site..."
+git push -f -q origin gh-pages > /dev/null
+
+
+echo "Deployed Successfully! <3"
+
+exit 0
